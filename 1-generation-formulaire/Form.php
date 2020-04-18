df0345475cc0b05ead8cdf87abc87b5a1a0d3078
Adrien <adrien.laigle35@gmail.com>
2020-04-18 17:37:20 +0200

    Activité 1 Formulaire

--- 1-generation-formulaire/Form.php
+++ 1-generation-formulaire/Form.php
@@ -1,4 +1,64 @@
 <?php
-class Form{
+
+class Form_exo{
+
+    private string $firstname;
+    private string $lastname;
+    private string $method;
+    private string $action;
+    private $fields = [];
+    public $options = [];
+
+    public function __construct($action, $method){
+
+        $this->action = $action;
+        $this->method = $method;
+    }
+
+    public function addTextField($fieldName, $fieldValue){ //Crée un input
+        
+       $this->fields[] = '<input name="'.$fieldName.'" value = "'.$fieldValue.'">';
+           
+    }
+
+    public function addSubmitButton($button){ //Crée un bouton
+
+        $this->button = '<br><br><input type="submit" value='.$button.'>';
+        return $this;
+    }
+
+    public function addTextArea($textarea){ //Crée une zone de texte
+        $this->textarea = '<br><br><textarea style=" resize : none"></textarea>';
+        return $this;
+
+    }
+
+    public function addSelect($options, $title){
+
+        $this->select = '<br><br><select id="'.$title.'" name="'.$title.'">'; //Ouvre le select
+
+        foreach ($options as $option) { //Sélectionne toutes les options rentrées dans la fonction
+            $options = $option;
+            $this->select .= '<option value = "'.$option.'" >'.$option.' </option>';
+        }
+
+        $this->select .= '</select>'; //Ferme le select
+    }
+
+    public function build(){ //Génère le formulaire
+
+        echo '<b>Voici le formulaire :</b>' ;
+
+        $build = '<br><br><form action = "'.$this->action.'" method = "'.$this->method.'">'; //Début du formulaire
+        foreach ($this->fields as $field) { //Sélectionne tous les input crées précedemment
+            $build .=  $field;
+        }                           // .= ajoute des éléments à la concaténation
+        $build .= $this->textarea; //Ajoute la textarea
+        $build .= $this->select; //Ajoute le select
+        $build .= $this->button; //Ajoute le bouton
+        $build .= '</form>'; //Fin du formulaire
+        return $build;
+
+    }
     
 }
--- 1-generation-formulaire/index.php
+++ 1-generation-formulaire/index.php
@@ -1,13 +1,19 @@
 <?php
 include 'Form.php';
 
-// Des variables par défaut pour vos tests.
 
-// YOUR CODE HERE
+$action = 'index.php';
+$method = 'post';
+$lastname = 'Laigle';
+$firstname = 'Adrien';
+$textarea = 'text';
 
-$form = new Form($action, $method);  // créer le début du formulaire
-$form->addTextField('nom',$nom); // créer un input de type texte avec comme valeur par défaut $nom
-$form->addTextField('prenom',$prenom); // créer un input de type texte avec comme valeur par défaut $prenom
+
+$form = new Form_exo($action, $method);  // créer le début du formulaire
+$form->addTextField('prenom', $firstname); // créer un input de type texte avec comme valeur par défaut $prenom
+$form->addTextField('nom',$lastname); // créer un input de type texte avec comme valeur par défaut $nom
+$form->addTextArea($textarea);// créer une textarea
+$form->addSelect([1=> 'Mr', 2=> 'Mme', 3=>'Autre'], 'title'); //créer une liste
 $form->addSubmitButton('Modifier'); //Créer un bouton pour soumettre le formulaire se nommant Modifier
 
 echo $form->build(); // générer le formulaire
